90f61c761dd93de187ae05f60e70a56d98727b38
iraryc <111704084+iraryc@users.noreply.github.com>
2024-06-07 14:40:35 -0700

    fun stuff
    fun stuff

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const monsterInput = document.getElementById("monsterInput");
 const searchButton = document.getElementById("searchButton");
 const monsterResult = document.getElementById("monsterResult");
 
+
 searchButton.addEventListener("click", () => {
   const monsterName = monsterInput.value.toLowerCase();
   if (monsterName) {
@@ -12,6 +13,33 @@ searchButton.addEventListener("click", () => {
     monsterResult.innerHTML = "Please enter a monster name.";
   }
 });
+
+function GrabMonsterDataFromAPI() {
+  fetch("https://www.dnd5eapi.co/api/monsters")
+      .then((response) => response.json())
+      .then((data) => {
+      coursesData = data;
+      console.log(data);
+      monsterDropDown(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching course data', error);
+      });
+}
+
+function monsterDropDown(data){
+  const monsterDropDown = document.getElementById("monsterDropDown");
+  const fragment = document.createDocumentFragment();
+  data.forEach(item => {
+      const option = document.createElement("option");
+      option.value = item.name;
+      option.text = item.name;
+      fragment.appendChild(option);
+  });
+  monsterDropDown.appendChild(fragment);
+
+}
+GrabMonsterDataFromAPI();
 
 function searchMonster(monsterName) {
   monsterResult.innerHTML = "Searching...";
@@ -32,6 +60,7 @@ function searchMonster(monsterName) {
             monsterResult.innerHTML = `
               <h2>${monsterData.name}</h2>
               <p><strong>Index:</strong> ${monsterData.index}</p>
+              <img src="https://www.enworld.org/attachments/pzo9500-10-chuffy-jpg.111691/" alt="monster image">
               <!-- You can display more monster details here -->
             `;
           })
